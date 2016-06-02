9a8c7d699c73d3d041cdc2a155e7fffe381e733e
Russell Wallace <russell.wallace@gmail.com>
2016-06-02 16:29:05 +0100

    test

--- test.scm
+++ test.scm
@@ -3,10 +3,4 @@
   ;; named failure continuation
   ((match-next v g+s (pat (=> failure) . body) . rest)
    (let ((failure (lambda ()
-                   (match-next v g+s . rest))))
-    ;; match-one analyzes the pattern for us
-    (match-one v pat g+s (match-drop-ids (begin . body)) (failure) ())))
-
-  ;; anonymous failure continuation, give it a dummy name
-  ((match-next v g+s (pat . body) . rest)
-   (match-next v g+s (pat (=> failure) . body) . rest))))
+                   (match-next v g+s . rest))))))))

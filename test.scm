523b7c2d5f3872740c7b5fb26af8b39edb458908
Russell Wallace <russell.wallace@gmail.com>
2016-06-02 16:29:45 +0100

    test

--- test.scm
+++ test.scm
@@ -1,6 +1,5 @@
 (define-syntax match-next
  (syntax-rules (=>)
-  ;; named failure continuation
   ((match-next v g+s (pat (=> failure) . body) . rest)
    (let ((failure (lambda ()
                    (match-next v g+s . rest))))))))
